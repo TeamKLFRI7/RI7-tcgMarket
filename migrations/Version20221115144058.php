c85d11e23cbd43243e179753d911c364d1e2b9fd
Fabien <fabien@hellocare.com>
2022-11-15 16:02:18 +0100

    suppression de la table messenger_message inutile dans le projet et créer avec la migration

--- migrations/Version20221115144058.php
+++ migrations/Version20221115144058.php
@@ -20,14 +20,12 @@ final class Version20221115144058 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_info CHANGE city city VARCHAR(50) NOT NULL, CHANGE adress address VARCHAR(55) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('ALTER TABLE user_info CHANGE city city VARCHAR(50) DEFAULT NULL, CHANGE address adress VARCHAR(55) DEFAULT NULL');
     }
 }
